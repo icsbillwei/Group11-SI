28e3dc8a8f8a2339e5520bf3a5669dadb2bc479b
icsbillwei <81921463+icsbillwei@users.noreply.github.com>
2024-10-18 18:25:34 -0400

    added login.html

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         user_input = request.form['user_input']
